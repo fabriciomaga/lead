28d9d8aecb91e8f9309f60f60e0bebd995710d53
fabriciomaga <46304127+fabriciomaga@users.noreply.github.com>
2023-01-17 11:35:22 -0300

    Initial commit

---
+++ inc/lets-talk.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lets-talk</title>
+    <link href="..\style.css" rel="stylesheet">
+</head>
+
+<body style="background-color: #ffd870;"> <!-- TIRAR AO FIM DA MONTAGEM-->
+
+<section class="background-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 style="font-weight: 800; text-decoration: underline;">Let's talk!</h2>
+                <ul>
+                    <form action="/action_page.php">
+                        <!--<label for="fname">First name:</label>-->
+                        <ul>
+                            <input class="input-top" type="text" id="fname" name="fname" placeholder="full name">
+                            <input class="input-top" type="text" id="email" name="email" placeholder="email" style="margin-left: -4px;"><br>
+                            <input class="input-bot" type="text" id="phone" name="phone" placeholder="telephone">
+                            <input class="input-bot" type="text" id="company" name="company" placeholder="company name" style="margin-left: -4px;">
+                        </ul>
+                        <!--<label for="lname">Last name:</label>-->
+                        <br><br>
+                        <input type="submit" value="Submit">
+                      </form> 
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</section>
+    
+</body>
+
+</html>
+<style>
+
+
+</style>
+
+<ul class="logo-home-1">
+                
+                    <ul class="text-top">
+                        Your <br>
+                        <span style="font-weight: 900;">communication </span><br>
+                        reinvented<br>
+                        <button class="btn-black-border">Let's talk</button>
+
+                    </ul>
+                </ul>
